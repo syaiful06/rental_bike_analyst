1713402b310ac123c38d9cecd58945f75ab9af91
moh syaifullah <mohammadsyaifullah80@gmail.com>
2024-10-27 15:25:58 +0700

    update file

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -38,7 +38,7 @@ def create_rfm_analysis(df):
 
     return rfm_df
 
-all_df = pd.read_csv("dashboard/main_data.csv")
+all_df = pd.read_csv("main_data.csv")
 
 all_df['dteday'] = pd.to_datetime(all_df['dteday'])
 
